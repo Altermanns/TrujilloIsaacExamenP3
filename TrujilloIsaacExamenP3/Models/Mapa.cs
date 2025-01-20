18ca194145f73c9c8871b81a146fbff2f3afc915
Altermanns <isaac.277.trujillo@gmail.com>
2025-01-20 07:32:58 -0500

    Creacion de modelo

---
+++ TrujilloIsaacExamenP3/Models/Mapa.cs
@@ -0,0 +1,19 @@
+﻿
+using SQLite;
+
+namespace TrujilloIsaacExamenP3.Models
+{
+    public class Mapa
+    {
+        [PrimaryKey, AutoIncrement]
+        public int IdBusqueda { get; set; }
+
+        public string Nombreoficial  { get; set; }
+
+        public string Región { get; set; }
+
+        public string LinkGoogle { get; set; }
+
+        public string NombreEstu { get; set; }
+    }
+}
