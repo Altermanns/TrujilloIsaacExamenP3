797cea6ac35f87ee49748a9a7dcad990b1ace661
laboratorio <laboratorio@UPOAULA10205>
2025-01-20 07:13:33 -0500

    Agregar archivos de proyecto.

---
+++ TrujilloIsaacExamenP3/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TrujilloIsaacExamenP3
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
